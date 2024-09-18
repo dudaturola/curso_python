2019206375f982f02778f564e11665b148cb4758
Maria Eduarda <149113013+dudaturola@users.noreply.github.com>
2024-09-18 14:38:00 -0300

    Update aula26.py

--- aula26.py
+++ aula26.py
@@ -1,10 +1,11 @@
 """
 Formatação básica de string
 s - string
-d e i - int
+d - int
 f - float
 .<número de dígitos>f
 x e X - Hexadecimal (ABCDEF0123456789)
+(Caractere)(><^)(quantidade)
 > - Esquerda
 < - Direita
 ^ - Centro
@@ -12,3 +13,9 @@ Sinal - + ou -
 Ex.: 0>-100,,1f
 Conversion flags - !r !s !a
 """
+variavel = 'ABC'
+print(f'{variavel}')
+print(f'{variavel: >10}')
+print(f'{variavel: <10}')
+print(f'{variavel: ^10}')
+print(f'{1000.48151312:,.1f}')
