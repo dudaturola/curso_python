e413b706a51851e6d941fb3e0f2150e932674fbb
Maria Eduarda <149113013+dudaturola@users.noreply.github.com>
2024-09-19 12:01:57 -0300

    aulas novas
    quero mudar a desgraça da minha vida logo nao aguento mais!!!!!!!!!!!!

--- aula26.py
+++ aula26.py
@@ -9,6 +9,7 @@ x e X - Hexadecimal (ABCDEF0123456789)
 > - Esquerda
 < - Direita
 ^ - Centro
+= - Força o numero a aparecer antes do zero
 Sinal - + ou -
 Ex.: 0>-100,,1f
 Conversion flags - !r !s !a
@@ -18,4 +19,5 @@ print(f'{variavel}')
 print(f'{variavel: >10}')
 print(f'{variavel: <10}')
 print(f'{variavel: ^10}')
-print(f'{1000.48151312:,.1f}')
+print(f'{1000.48151312:+,.1f}')
+print(f'O hexadecial de 1500 é {1500:08X}')
