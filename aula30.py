7d1dd9feb715487d341150dcce4709438c9462d5
Maria Eduarda <149113013+dudaturola@users.noreply.github.com>
2024-09-20 09:13:01 -0300

    Update aula30.py

--- aula30.py
+++ aula30.py
@@ -4,14 +4,22 @@ Muitas codições no mesmo if (ruim)
     <- Contagem de complexidade (ruim)
 """
 velocidade = 61 #velocidade atual do carro
-local_carro = 90 #local em que o carro está na estrada
+local_carro = 99 #local em que o carro está na estrada
 
 RADAR_1 = 60 #velocidade máxima do radar
 LOCAL_1 = 100 #local onde o radar 1 está
 RADAR_RANGE = 1 #A distancia onde o radar pega
 
-if velocidade > RADAR_1:
+vel_carro_passou_radar_1 = velocidade > RADAR_1
+carro_passou_radar_1 = local_carro >= (LOCAL_1 - RADAR_1) and\
+    local_carro <= (LOCAL_1 + RADAR_1)
+carro_multado_radar_1 = carro_passou_radar_1 and vel_carro_passou_radar_1
+
+if vel_carro_passou_radar_1:
     print('Você está acima do limite permitido')
 
-if local_carro >= (LOCAL_1 + RADAR_RANGE) and (LOCAL_1 - RADAR_RANGE):
+if carro_passou_radar_1:
+    print('Carro passou radar 1')
+
+if carro_multado_radar_1:
     print('Carro multado em radar 1')
