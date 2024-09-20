20a5bba3eb93240ccd404e4896c9d7a0b4f92e76
Maria Eduarda <149113013+dudaturola@users.noreply.github.com>
2024-09-20 08:15:52 -0300

    Update aula28.py

--- aula28.py
+++ aula28.py
@@ -15,21 +15,25 @@ Se nada for digitado em nome ou idade:
 """
 nome = input('Digite o seu nome: ')
 idade = input('Digite a sua idade: ')
+if nome and idade:
+    print(f'Seu nome é {nome}')
+    print(f'Seu nome invertido é {nome[::-1]}')
 
-if (nome,idade) == '':
+    if " " in nome:
+        print('Seu nome contém espaço.')
+    else:
+        print('Seu nome não tem espaço.')
+        
+    print(f'Seu nome contém {len(nome)} caracteres.')
+    print(f'A primeira letra do seu nome é {nome[0]}')
+    print(f'A última letra do seu nome é {nome[-1]}')
+
+else:
     print('Desculpe, você deixou espaços vazios!')
 
-print(f'Seu nome é {nome}')
-print(f'Seu nome invertido é {nome[::-1]}')
 
-if " " in nome:
-    print('Seu nome contém espaço.')
-else:
-    print('Seu nome não tem espaço.')
 
-caracteres = len(nome)
-print(f'Seu nome contém {caracteres} caracteres.')
 
-print(f'A primeira letra do seu nome é {nome[0]}')
-print(f'A última letra do seu nome é {nome[::-caracteres]}')
 
+
+    
