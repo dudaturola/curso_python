c5ba2353f9ef1df766806d747486bcc294eef55d
Maria Eduarda <149113013+dudaturola@users.noreply.github.com>
2024-09-22 22:11:42 -0300

    aulassss

---
+++ aula33.py
@@ -0,0 +1,6 @@
+string = 'nada'
+print(string)
+"""
+Essa aula ele disse para olhar a documentação do python,
+ pois é uma boa pratica de programação
+"""
---
+++ aula34.py
@@ -0,0 +1,5 @@
+"""
+Repetições
+while(enquanto)
+executa uma ação enquanto uma condição for verdadeira
+"""
