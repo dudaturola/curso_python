a7faa4e83852910b145e0b5cb373daf05c876836
Maria Eduarda <149113013+dudaturola@users.noreply.github.com>
2024-09-23 14:18:47 -0300

    aula sobre while

--- aula34.py
+++ aula34.py
@@ -2,4 +2,15 @@
 Repetições
 while(enquanto)
 executa uma ação enquanto uma condição for verdadeira
+Loop infinito -> Quando um codigo não tem fim
 """
+condicao = True
+
+while condicao:
+    nome = input('Qual o seu nome: ')
+    print(f'Seu nome é {nome}')
+
+    if nome == 'sair':
+        break
+
+print('Acabou!')
