32170ea9c5724ff5c2eaeac1f9a475872cd7b419
Maria Eduarda <149113013+dudaturola@users.noreply.github.com>
2024-10-04 08:59:34 -0300

    aulas 04/10

---
+++ aula38.py
@@ -0,0 +1,20 @@
+"""
+Repetiçoes
+while(enquanto)
+executa uma ação enquanto uma condiçao for verdadeira
+loop infinito -> quando um código nao tem fim
+"""
+
+qtd_linhas = 5
+qtd_colunas = 5
+
+linha = 1
+
+while linha <= qtd_linhas:
+    coluna = 1
+    while coluna <= qtd_colunas:
+        print(f'{linha=} {coluna=}')
+        coluna += 1
+    linha += 1
+
+print('Acabou')
