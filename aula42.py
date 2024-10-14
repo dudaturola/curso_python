d2801b305c94723385f84331f4139e078551a417
Maria Eduarda <149113013+dudaturola@users.noreply.github.com>
2024-10-13 21:10:09 -0300

    aulas novas

--- aula42.py
+++ aula42.py
@@ -1,29 +1,25 @@
-frase = 'aaooo'
+frase = 'aaaooo'
 
 i = 0
 qtd_apareceu_mais_vezes = 0
-letra_apareceu_mais_vezes = ''
+letra_que_apareceu_mais_vezes =''
 
 while i < len(frase):
     letra_atual = frase[i]
 
     if letra_atual == ' ':
-        i += 1  
+        i += 1
         continue
 
+    qtd_atual = frase.count(letra_atual)
 
-    qtd_apareceu_mais_vezes_atual = frase.count(letra_atual)
-
-    if qtd_apareceu_mais_vezes < qtd_apareceu_mais_vezes_atual:
-        qtd_apareceu_mais_vezes = qtd_apareceu_mais_vezes_atual
-        letra_apareceu_mais_vezes = letra_atual
+    if qtd_apareceu_mais_vezes < qtd_atual:
+        qtd_apareceu_mais_vezes = qtd_atual
+        letra_que_apareceu_mais_vezes = letra_atual
 
     i += 1
-
-    print(
-    'A letra que apareceu mais vezes foi '
-    f'"{letra_apareceu_mais_vezes}" que apareceu '
-    f'{qtd_apareceu_mais_vezes}x'
-    )
-
-    break
+    
+print('A letra que apareceu mais vezes foi'
+        f' "{letra_que_apareceu_mais_vezes}" que apareceu '
+        f'{qtd_apareceu_mais_vezes}x'
+        )
