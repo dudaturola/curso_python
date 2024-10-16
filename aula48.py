6d37ec70ab64f52500d8f8f92949a6f9e9423113
Maria Eduarda <149113013+dudaturola@users.noreply.github.com>
2024-10-16 08:07:34 -0300

    ++

---
+++ aula48.py
@@ -0,0 +1,17 @@
+"""
+Listas em Python
+Tipo list - Mutável
+Suporta vários valores de qualquer tipo
+Conhecimentos reutilizáveis - índices e fatiamento
+Métodos úteis: append, insert, pop, del, clear, extend, +
+"""
+#      +01234
+#      -54321
+string = 'ABCDE' # 5 caracteres
+
+# print(bool(lista)) #falsy
+# print(lista, type(lista))
+
+#         0    1      2              3  4
+lista = [123, True, 'Maria Eduarda', 1.2,[]]  
+print(lista[2], type(lista[4]))
---
+++ aula49.py
@@ -0,0 +1,18 @@
+"""
+Listas em Python
+Tipo list - Mutável
+Suporta vários valores de qualquer tipo
+Conhecimentos reutilizáveis - índices e fatiamento
+Métodos úteis:
+    append, insert, pop, del, clear, extend, +
+Create Read Update  Delete
+Criar, ler, alternar, apagar = listas[i] (CRUD)
+"""
+#         0   1   2   3   4   5
+lista = [10, 20, 30, 40, 50, 60]
+# lista[2] = 300
+# del lista[2]
+# print(lista)
+# print(lista[2])
+lista.append(70)
+print(lista)
