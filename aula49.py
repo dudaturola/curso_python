9b482d881dad767d4c619a8b09d582bb59b8a282
Maria Eduarda <149113013+dudaturola@users.noreply.github.com>
2024-10-20 21:10:06 -0300

    Update aula49.py

--- aula49.py
+++ aula49.py
@@ -14,5 +14,13 @@ lista = [10, 20, 30, 40, 50, 60]
 # del lista[2]
 # print(lista)
 # print(lista[2])
-lista.append(70)
+lista.append(70) #append = adiciona um valor
+lista.pop() #pop = remove o ultimo valor
+lista.append(80)
+print(lista)
+lista.insert(0, 5)
+"""o insert primeiro seleciona o indice, no caso o 0; 
+depois oq quer colocar no lugar, no caso o 5"""
+
+#lista.clear() #limpa a lista
 print(lista)
