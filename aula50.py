86a5637ccf6545241d9db2ba313f9e737b25392f
Maria Eduarda <149113013+dudaturola@users.noreply.github.com>
2024-11-10 21:54:19 -0300

    mais

---
+++ aula50.py
@@ -0,0 +1,10 @@
+"""
+for in com listas
+"""
+lista = ['Maria', 'Helena', 'Luiz']
+lista.append('Joao')
+
+indices = range(len(lista))
+
+for indice in indices:
+    print(indice, lista[indice], type(lista[indice]))
