58ee77557b6eee6548254283ecfbf2e85fa37b7d
Maria Eduarda <149113013+dudaturola@users.noreply.github.com>
2024-12-13 07:05:56 -0300

    aulas

--- aula69.py
+++ aula69.py
@@ -14,11 +14,11 @@ ser a mesma no escopo interno.
 x = 1
 
 def escopo():
-    global x
+    # global x
     x = 10
 
     def outra_funcao():
-        global x
+        # global x
         x = 11
         y = 2
         print(x,y)
---
+++ aula70.py
@@ -0,0 +1,14 @@
+"""
+Retorno de valores das funções (return)
+"""
+
+def soma(x,y):
+    return x + y
+
+
+# variavel = soma(1,2)
+# variavel = int('1')
+# print(variavel)
+soma1 = soma(2,2)
+soma2 = soma(3,3)
+print(soma1 + soma2)
