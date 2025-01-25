51d8c92916386096afbb16eb621bef27d5ad802f
Maria Eduarda <149113013+dudaturola@users.noreply.github.com>
2025-01-24 22:16:13 -0300

    list comprehension

--- aula89.py
+++ aula89.py
@@ -2,6 +2,11 @@
 # List comprehension é uma forma rápida para criar listas
 # a partir de iteráveis.
 # print(list(range(10)))
+import pprint
+
+def p(v):
+    pprint.pprint(v, sort_dicts=False, width=40)
+
 lista = []
 for numero in range(10):
     lista.append(numero)
@@ -26,4 +31,13 @@ novos_produtos = [
     for produto in produtos
 ]
 # print(novos_produtos)
-print(*novos_produtos, sep='\n')
+# print(novos_produtos)
+# p(novos_produtos)
+# lista = [n for n in range(10) if n < 5]
+novos_produtos = [
+    {**produto, 'preco': produto['preco'] * 1.05}
+    if produto['preco'] > 20 else {**produto}
+    for produto in produtos
+    if produto['preco'] >10
+]
+p(novos_produtos)
